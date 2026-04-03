8447b6499c6e39902edd574728b4ce1b21526305
ONURBTHECOOL <rotatingfun@gmail.com>
2026-04-03 14:00:06 -0700

    Update URL and improve error messages in webcheck.py

--- webcheck.py
+++ webcheck.py
@@ -1,10 +1,10 @@
 import requests
 from time import sleep
 
-url = "https://www.sardinesclub.org"
-new_content = ""
+url = "https://www.rotators.biz"
+new_content = "Please fetch the content first \x1B[3mThen\x1B[0m Show the content"
 old_content = ""
-url_home = f"{url}/Sardines%20Official%20Website%20About%20Us"
+url_home = f"{url}/"
 checkFrequency = 20
 # noinspection PyRedeclaration
 old_content = ""
@@ -21,9 +21,10 @@ def fetch(address):
             else:
                 print("error")
         except requests.exceptions.ConnectionError:
-            print("Sorry, The Website You Tried To Access Dose Not Exist!!")
+            print("Sorry, The Website You Tried To Access Does Not Exist!!")
     except requests.exceptions.MissingSchema:
-        print("You are stupid to think that you can put a url with no https:// on it!")
+        print("You are stupid to think that you can put a url with no https:// on it! Go add one with setup:change-url")
+        interface(setup_is_true=False)
 
 
 def store(old):
@@ -44,7 +45,7 @@ def store(old):
             print("Error while connecting to site.\nCheck your internet connection.")
         else:
             print("Successfully connected to site.")
-            ask = input("Type Y to see the page content, or N to continue.\n")
+            ask = input("Type Y to see the page content, or N to continue.")
             if ask == "Y":
                 print(old_content)
 
@@ -54,7 +55,8 @@ def setup(command):
     global url
     global url_home
     if command == "change-url":
-        url = input("Enter new url\nsetup:")
+        url = input("Enter new url\nsetup/change-url:")
+        url_home = f"{url}/"
     elif command == "change-check-frequency":
         checkFrequency = int(input("Enter new check frequency\nsetup:"))
     elif command == "change-home-directory":
@@ -62,6 +64,7 @@ def setup(command):
         url_home = f"{url}{input(prompt)}"
     else:
         print("cmd not valid")
+        interface(True)
 def compare():
     global new_content
     global old_content
@@ -80,35 +83,45 @@ def check():
         compare()
         sleep(checkFrequency)
 
-def interface():
+def interface(setup_is_true):
     cmd = input()
+    if setup_is_true:
+        setup_cmd = input("What would you like to configure?\nsetup:")
+        setup_is_true(setup_cmd)
     if cmd == "fetch-old":
         store(True)
-        interface()
+        interface(setup_is_true=False)
     if cmd == "fetch-new":
         store(False)
-        interface()
+        interface(setup_is_true=False)
     if cmd == "quit":
         exit()
     if cmd == "show-new":
         print()
-        interface()
+        interface(setup_is_true=False)
     if cmd == "bald":
         print("Thank You For Your Baldness\n")
-        interface()
+        interface(setup_is_true=False)
     if cmd == "setup":
         setup_cmd = input("What would you like to configure?\nsetup:")
-        setup(setup_cmd)
-        interface()
+        setup_is_true(setup_cmd)
+        interface(setup_is_true=False)
     if len(cmd) > 22:
         print("cmd not valid")
     if cmd == "check":
         check()
-        interface()
+        interface(setup_is_true=False)
+    if "setup" in cmd:
+        cmd.replace("setup", "")
+        setup_is_true(cmd)
+        interface(setup_is_true=False)
     else:
         print("cmd not valid")
-        interface()
+        interface(setup_is_true=False)
 
 
-print("welcome to the website checker")
-interface()
+try:
+    print("welcome to the website checker")
+    interface(setup_is_true=False)
+except KeyboardInterrupt:
+    print("Dont leave me!")
